70b1f736d0b2e658226669ad9441378c9eaaddc8
Thiago Paixão <tspaixao@sp.r7.com>
2015-03-16 16:19:36 -0300

    adicionado imgmin, não testado ainda

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 var grunt = function(grunt) {
-  var stylusPathArray = ['assets/style/main.styl','assets/style/custom/**/*.styl'];
+  var stylusPathArray = ['assets/style/main.styl', 'assets/style/custom/**/*.styl'];
   var jsPathArray = ['assets/js/custom/main.js'];
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -8,13 +8,13 @@ var grunt = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: ['assets/js/custom/main.js',],
+        src: jsPathArray,
         dest: 'build/js/main.min.js'
       }
     },
     jasmine: {
       pivotal: {
-        src: 'assets/js/**/*.js',
+        src: jsPathArray,
         options: {
           specs: 'assets/specs/*Spec.js',
           helpers: 'assets/specs/*Helper.js'
@@ -34,14 +34,69 @@ var grunt = function(grunt) {
           'build/style/style.min.css': stylusPathArray
         }
       }
+    },
+    watch: {
+      scripts: {
+        files: '<%= uglify.build.src %>',
+        tasks: ['uglify'],
+        options: {
+          spawn: false
+        }
+      },
+      styles: {
+        files: stylusPathArray,
+        tasks: ['stylus'],
+        options: {
+          spawn: false
+        }
+      },
+      images: {
+        files: 'assets/img/**/*.{png,jpg,gif}',
+        tasks: ['imagemin'],
+        options: {
+          spawn: false
+        }
+      }
+      
+    },
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        options: {
+          cache: false
+        },
+      },
+      files: [{
+        // Set to true to enable the following options…
+        expand: true,
+        // cwd is 'current working directory'
+        cwd: 'build/img/',
+        src: ['assets/img/**/*.png'],
+        // Could also match cwd line above. i.e. project-directory/img/
+        dest: 'build/img/',
+        ext: '.png'
+      }]
+
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'build/img/',
+          src: ['assets/img/**/*.{png,jpg,gif}'],
+          dest: 'build/img/'
+        }]
+      }
     }
   });
+
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  
-  grunt.registerTask('default',['stylus','uglify'])
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['watch']);
 };
 
 module.exports = grunt;
